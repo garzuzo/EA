0121b4ab51ea496b26b0a5453c9982a002f3ddf4
garzuzo <garzuzo@garzuzo-V5-171>
2018-03-26 22:09:53 -0500

    changes

--- EquivalenciaAutomatas/src/Interfaz/PanelMealy.java
+++ EquivalenciaAutomatas/src/Interfaz/PanelMealy.java
@@ -1,5 +1,24 @@
 package Interfaz;
 
-public class PanelMealy {
+import javax.swing.*;
 
+public class PanelMealy extends JPanel{
+
+
+	
+	public PanelMealy() {
+		
+		JLabel lEstado=new JLabel("Estados");
+		JLabel lStim1=new JLabel("Estimulo 1");
+		JLabel lStim2=new JLabel("Estimulo 2");
+		JLabel lResp=new JLabel("Response");
+		
+		
+		
+	}
+	
+	public void generateFields(int numStatesM1,int numStatesM2) {
+		
+		
+	}
 }
--- EquivalenciaAutomatas/src/Interfaz/PanelMoore.java
+++ EquivalenciaAutomatas/src/Interfaz/PanelMoore.java
@@ -1,5 +1,49 @@
 package Interfaz;
 
-public class PanelMoore {
+import javax.swing.*;
 
+import Mundo.Estado;
+
+import java.awt.GridLayout;
+import java.util.*;
+public class PanelMoore extends JPanel{
+
+	
+	ArrayList<JTextField> arrM1;
+	ArrayList<JTextField> arrM2;
+	public PanelMoore(int tam) {
+		
+		
+		
+		setLayout(new GridLayout(tam, 10));
+		
+		JLabel lEstado=new JLabel("Estados");
+		JLabel lStim1=new JLabel("Estimulo 1");
+		JLabel lStim2=new JLabel("Estimulo 2");
+		JLabel lResp=new JLabel("Response");
+		
+		JButton bAceptM1=new JButton("Aceptar M1");
+		JButton bAceptM2=new JButton("Aceptar M2");
+	}
+	
+	public void generateFields(int numStatesM1,int numStatesM2) {
+		
+		for (int i = 0; i < numStatesM1; i++) {
+			JTextField nAct=new JTextField("");
+			arrM1.add(nAct);
+		}
+		
+		add(new JLabel("____________"));
+		add(new JLabel("____________"));
+		add(new JLabel("____________"));
+		
+		for (int i = 0; i < numStatesM1; i++) {
+			
+			
+			JTextField nAct=new JTextField("");
+			arrM2.add(nAct);
+		}
+	
+		
+	}
 }
